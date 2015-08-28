298722016b58987f4a64c5d552016ffe1283e6c3
dani <dani@dani-pc.fritz.box>
2015-08-28 12:31:42 +0200

    clean up code: moved hdf5-stuff to Prints.h, moved initialisation mainly to main.cpp, moved to new time-computation (leave out global time of the bunch), integrated some functions of Compute.h, removed useless parameters and variables

--- src/phaseBunch/Compute.h
+++ src/phaseBunch/Compute.h
@@ -1,48 +1,17 @@
 #ifndef COMPUTE_H
 #define COMPUTE_H
 
-//~ #include <math.h>
-#include <cmath>
+#include <math.h>
 #include <stdio.h>
-
-#include <hdf5.h>
-#include <hdf5_hl.h>
 
 #include <assert.h>
 
-#include "Params.h"
+#include "Prints.h"
 
-//#include <omp.h>
 
 #ifndef SOL
 #define SOL 299792458
 #endif
-
-inline long double computeGamma(long double px, long double m) {
-    return sqrtl(1 + (px*px) / (m*m));//changed from - to + as E^2 = E0^2 + (pc)^2 and E = gamma * E0
-}
-
-inline long double computeVi(long double pi, long double gamma, long double m) {
-    //v_i = p_i * c / (gamma * m)
-    return pi / (gamma * m);//removed product with SOL as px stores energy, not energy over c TODO: check this
-}
-
-
-void computeLorentz( long double q, long double x, long double *F) {
-    //~ *F = q *  Ex(x);
-    *F = q * 2.193245422464302e-06*x;
-}
-
-void computeNewImpulse( long double dt, long double *px, long double F, long double gamma) {
-    //old computation of new momentum: no energy conservation (magnetic field also accelerates)
-    *px = *px + 3e8 * F * dt * gamma;
-}
-
-void computeNewPosition(
-    long double dt, long double *x, long double px, long double F, long double gamma, long double m
-) {
-    *x = *x + SOL * px / ( gamma * m ) * dt + 1.0 / 2.0 * dt*dt * F * SOL * SOL / ( gamma * m );
-}
 
 void updateParticle(
     long double dt,
@@ -52,13 +21,12 @@ void updateParticle(
 
     long double gamma,F;
 
-    gamma = computeGamma(*px, m);
-    computeLorentz(q, *x, &F);
-    
-    //~ printf("F = %Le\n", F);
+    gamma = sqrtl(1 + ((*px)*(*px)) / (m*m));
+    F = q * 2.193245422464302e-06* (*x); //computes Lorentz-force
 
-    computeNewPosition(dt, x, *px, F, gamma, m);
-    computeNewImpulse(dt, px, F, gamma);
+	//update position and momentum
+    *x = *x + SOL * (*px) / ( gamma * m ) * dt + 1.0 / 2.0 * dt*dt * F * SOL * SOL / ( gamma * m );
+    *px = *px + 3e8 * F * dt * gamma;
     
 }
 
@@ -66,100 +34,29 @@ void compute(
     long double t_start, long double t_end, long double dt,
     long double x[], long double px[], 
     long double m[], long double q[],
-    int len, int *k,
-    long double beamspeed, long double circumference
+    int len,
+    long double beamspeed, long double circumference,
+    hid_t* dataset
 ) {
-
     int i,j;
-    
-    //for memory-reasons, whenever the particle-positions are to be
-    //stored, they are directly written into the resulting file.
-    //So - a lot of hdf5-stuff to do here
-	hsize_t dim[1] = { (long long unsigned int) len }; //length of a chunk, which is the number of particles
-	hsize_t maxdim[1] = {H5S_UNLIMITED};
-	hsize_t offset[1];
-	hsize_t size[1] = { 0 };
-	const hsize_t RANK = 1; //dimension of data, which is 1
-	
-	hid_t   file;                          /* handles */
-    hid_t   dataspace, dataset;  
-    hid_t   filespace, memspace;
-    hid_t   prop;
-    
-    //create file
-    file = H5Fcreate ("result.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);    
-    
-    //create data-space w/o dimension-limit
-    dataspace = H5Screate_simple (RANK, dim, maxdim);
-
-    /* Modify dataset creation properties, i.e. enable chunking  */    
-    prop = H5Pcreate (H5P_DATASET_CREATE);
-    H5Pset_chunk (prop, RANK, dim);
-    /* Create a new dataset within the file using chunk 
-       creation properties.  */
-    dataset = H5Dcreate2 (file, "signal", H5T_NATIVE_LDOUBLE, dataspace,
-                         H5P_DEFAULT, prop, H5P_DEFAULT);
-	
-	long double *tmp; tmp = (long double*) malloc(len * sizeof(long double));
     long double t;
     for( t = t_start,j = 1; t < t_end - dt; t += dt) {
 
-#pragma omp parallel for default(none) private(i) shared(len, x, px, dt, m, q, t, beamspeed) if(len > 4)
+#pragma omp parallel for default(none) private(i) shared(len, x, px, dt, m, q) if(len > 4)
         for(i = 0; i < len; i++) {
 			updateParticle(dt, &(x[i]), &(px[i]), q[i], m[i]);
         }
-	//	printf("%Le\n", x[0]);
         
         //check, whether sync-particle passed the detector
         if( t * beamspeed > j * circumference ) {
 			//store the current particle-positions, corrected by the current time
 			//(the particle's offset to the sync-particles are computed)
-			
-		    /* Extend the dataset. Dataset becomes 10 x 3  */
-			size[0] = size[0]+ dim[0];
-			assert(size[0] == j * dim[0]);
-			H5Dset_extent (dataset, size);
-
-			/* Select a hyperslab in extended portion of dataset  */
-			filespace = H5Dget_space(dataset);
-			offset[0] = (j-1) * dim[0];
-			H5Sselect_hyperslab (filespace, H5S_SELECT_SET, offset, NULL,
-										  dim, NULL);  
-
-			/* Define memory space */
-			memspace = H5Screate_simple (RANK, dim, NULL);
-
-			#pragma omp parallel for default(none) private(i) shared(tmp, t, px, m, x, len, beamspeed)  if(len > 4)
-			for(i = 0; i < len; i++) {
-				tmp[i] = t + x[i] / beamspeed;
-			}
-
-			/* Write the data to the extended portion of dataset  */
-			H5Dwrite (dataset, H5T_NATIVE_LDOUBLE, memspace, filespace,
-							   H5P_DEFAULT, tmp);
-						
-			
-			H5Sclose (memspace);
-			H5Sclose (filespace);
-						
+		    SaveChunk( dataset, j, len, beamspeed, &x);
 			j++;
 		}
 
     }
-    
-    /* Close resources */
-    H5Dclose (dataset);
-    H5Pclose (prop);
-    H5Sclose (dataspace);
-    
-    
-    size[0] = 6; long double turns[] = { (long double) j - 1, (long double) len, dt , t_start, t_end, circumference / beamspeed};
-    H5LTmake_dataset(file,"/params",1,size,H5T_NATIVE_LDOUBLE,turns);
-    
-    H5Fclose (file);
 
 	printf("j = %i\n", j);
-
-    *k = j;
 }
 #endif
--- src/phaseBunch/Init.h
+++ src/phaseBunch/Init.h
@@ -1,105 +1,45 @@
 #ifndef INIT_H
 #define INIT_H
 
-#include <hdf5.h>
-#include <hdf5_hl.h>
-
-#include <chrono>
 #include <stdlib.h>
 #include <stdio.h>
 #include <random>
 
-#include <cmath>
+#include <math.h>
 
-#ifndef SOL
-#define SOL 299792458.0f
-#endif
-#ifndef ELEMENTARY_CHARGE
-#define ELEMENTARY_CHARGE (1.602176565e-19)
-#endif
+#include "Prints.h"
 
 typedef struct part{
-
   long double *x, *px, *q, *m;
-
-
 } particle;
 
-void init(	long double* t_start, long double *t_end, long double *dt,
-            long double *beamspeed, long double *circumference,
-            int *length,
-            particle *p,
-			long double *freq
-) {
-    //loop-variable for later use
-    int i;
-
-    //initialise length (from array length): number of particles
-    *length = 1e3;
-    
-    *t_start = 0;//in seconds
-    *t_end   = 1e-6;//in seconds
-    *dt      = 1e-9;//in seconds
-    
-    *beamspeed = 0.467 * SOL;
-    *circumference = 108.5;//m
-
-	const long double amplitude = 5;//unit: meter
-    *freq = 1e2;//unit: hearts
-    const long double omega = 2 * M_PI * (*freq);
-    const long double deltaOmega = 2 * M_PI * 1e5 * 0;
+void init(	int length, particle *p, long double deltaP,
+			hid_t *file, hid_t *dataset) {
 
     std::random_device generator;
     //get the first standard distribution: mean, standart deviation
     //as the momentum is expected in eV: both quantities also in eV
-    /*
-     * Gaussian: E_total = E_beam + E_bucket. E_bucket = 0 for the syn-
-     * cronous particle. as the energie in the bucket here is distributed,
-     * the mean is 0 and the standard deviation is equal to the deviation
-     * of the energie, whereas delta E / E = beta**2 * delta p / p with
-     * delta p / p = 1e-5. E = 122 MeV/u, 12C3+ are considered
-     */
     std::normal_distribution<long double> position(0, 0);
-    std::normal_distribution<long double> circularFrequency(omega, deltaOmega);
+    std::normal_distribution<long double> momentum(0, deltaP);
     
-    //allocate memory for each component of position
-    p->x = (long double*) malloc(sizeof(long double) * (*length));
-
-    //allocate memory for each component of momentum
-    p->px = (long double*) malloc(sizeof(long double) * (*length));
-
-    //allocate memory for mass and charge
-    p->q = (long double*) malloc(sizeof(long double) * (*length));
-    p->m = (long double*) malloc(sizeof(long double) * (*length));
+    //allocate memory for position, momentum, mass & charge
+    p->x  = (long double*) malloc(sizeof(long double) * length);
+    p->px = (long double*) malloc(sizeof(long double) * length);
+    p->q  = (long double*) malloc(sizeof(long double) * length);
+    p->m  = (long double*) malloc(sizeof(long double) * length);
 
     //initialise each parameter for each particle
-    for(i=0; i < (*length); i++) {
+    for( int i=0; i < length; i++) {
 		p->x[i]  = position(generator);//in m
         p->q[i] = -1;//in number of the elementary charge
         p->m[i] = 0.5e6;//in eV
-        p->px[i] = 5.2;
+        p->px[i] = momentum(generator);
         
     }
     
-    printf("paricel 1: x = %Le\n", p->x[0]);
-    printf("paricel 1: p = %Le\n", p->px[0]);
-    
-    double *tmp;
-    tmp = (double*) malloc(sizeof(double) * (*length));
-    for(i=0; i < (*length); i++) { tmp[i] = (double) p->px[i]; }
-    
-    hid_t file_id;
-	hsize_t dims[1];
-    file_id = H5Fcreate("distribution.h5",H5F_ACC_TRUNC,H5P_DEFAULT,H5P_DEFAULT);
-    dims[0] = (*length);
-	H5LTmake_dataset(file_id,"/signal",1,dims,H5T_NATIVE_DOUBLE,tmp);
-    dims[0] = 2;
-    tmp[0] = (*dt);  tmp[1] = (double) *freq; 
-    H5LTmake_dataset(file_id,"/params",1,dims,H5T_NATIVE_DOUBLE,tmp);
-    H5Fclose(file_id);
-	
-    
-    free(tmp);
+    printInitDistribution(&(p->px), length);
+    InitResultFile(file, dataset, length);
+  
 }
 
 
