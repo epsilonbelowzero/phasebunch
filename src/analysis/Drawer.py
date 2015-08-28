f5bb688c4e48d7ed371220c8f3386e6e4e8d6f9d
dani <dani@dani-pc.fritz.box>
2015-08-28 12:34:23 +0200

    updated Drawer to new requests, print of frequencies possible

--- src/analysis/Drawer.py
+++ src/analysis/Drawer.py
@@ -12,14 +12,18 @@ if len(sys.argv) < 2:
 	print("You forgot the file!")
 	sys.exit(1)
 
-x,y = process_signal.process(sys.argv[1])
+x,y, T = process_signal.process(sys.argv[1])
 
 fig = plt.figure()
 
 ax = fig.add_subplot(111)
 
 ax.plot(x, y, ".")
-#~ ax.plot(data[:i] if not singleCol else data, 'ro')
 ax.set_xlabel("$\mathbf{n \cdot dt}$")
+
+print(1 / (T + x))
+
+#~ ax = fig.add_subplot(212)
+#~ ax.plot( , y);
 
 plt.show()
