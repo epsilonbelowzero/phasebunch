e14b50fde922c13c5d09fd17f681ea982f619a08
epsilonbelowzero <duda.land@gmail.com>
2015-09-11 16:58:05 +0200

    changed output to bar-output

--- src/analysis/Drawer.py
+++ src/analysis/Drawer.py
@@ -18,12 +18,14 @@ fig = plt.figure()
 
 ax = fig.add_subplot(111)
 
-ax.plot(x, y, ".")
+ax.bar(x*1e9, y, .5)
 ax.set_xlabel("$\mathbf{n \cdot dt}$")
+ax.set_yscale("log")
 
 print(1 / (T + x))
 
-#~ ax = fig.add_subplot(212)
-#~ ax.plot( , y);
+ax = fig.add_subplot(212)
+ax.set_yscale("log")
+ax.bar( 1/(T + x), y, .5);
 
 plt.show()
