93a51b3d0e4e55ba6a91f79fa589bb3f7bf79219
s0vereign <maxboehme@live.de>
2015-09-29 10:27:41 +0200

    Added one line

--- src/analize/Reader.h
+++ src/analize/Reader.h
@@ -2,14 +2,16 @@
 #define READER_H
 
 #include "hdf5.h"
+#include "hdf5_hl.h"
 #include <stdio.h>
 
 
 
 void read(char** argc, char* argv, ){
 
-
-
+	
+ 	hid_t file,dataset,filespace,memspace,dims[2]; // Create all the necessary attributes to read the file!
+	
 
 
 
